6c103af49482ed5dcd6bd78dc6b87a3fdbda1404
Jack Kelly <xJaquar7@gmail.com>
2020-07-20 13:42:19 +1200

    Added Gameplay.cs

---
+++ Gameplay.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamPaperScissorsRock2020
+{
+    static class Gameplay
+    {
+        public static string ComputerChoice()
+        {
+            // Returns either Paper, Scissors or Rock (Random)
+            Random myRandom = new Random();
+            string[] Guess = { "", "Paper", "Scissors", "Rock" };
+            return Guess[myRandom.Next(1, 4)];
+        }
+
+        public static string Play(string Human, string Comp)
+        {
+            // Draw Logic
+            if (Human == Comp)
+            {
+                return "Draw";
+            }
+            // Winning Logic
+            else if (Human == "Paper")
+            {
+                if (Comp == "Scissors")
+                {
+                    return "Comp Win";
+                }
+
+                else if (Comp == "Rock")
+                {
+                    return "Human Wins";
+                }
+            }
+            else if (Human == "Scissors")
+            {
+                if (Comp == "Rock")
+                {
+                    return "Comp Win";
+                }
+
+                else if (Comp == "Paper")
+                {
+                    return "Human Wins";
+                }
+            }
+            else if (Human == "Rock")
+            {
+                if (Comp == "Paper")
+                {
+                    return "Comp Win";
+                }
+
+                else if (Comp == "Scissors")
+                {
+                    return "Human Wins";
+                }
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -3,19 +3,63 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
 
 namespace XamPaperScissorsRock2020
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        ImageView GamePic;
+        TextView txtMainMsg;
+        Button btnPlay;
+        string Human, Name, NPC;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Init();
         }
+
+        void Init()
+        {
+            txtMainMsg = FindViewById<TextView>(Resource.Id.txtMainMsg);
+            GamePic = FindViewById<ImageView>(Resource.Id.imgAnswer);
+            btnPlay = FindViewById<Button>(Resource.Id.btnPlay);
+
+            RadioButton rbnPaper = FindViewById<RadioButton>(Resource.Id.rbnPaper);
+            RadioButton rbnScissors = FindViewById<RadioButton>(Resource.Id.rbnScissors);
+            RadioButton rbnRock = FindViewById<RadioButton>(Resource.Id.rbnRock);
+
+            rbnPaper.Click += Rbn_Click;
+            rbnScissors.Click += Rbn_Click;
+            rbnRock.Click += Rbn_Click;
+
+            btnPlay.Click += btnPlay_Click;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            string NPC = Gameplay.ComputerChoice();
+            string Result = Gameplay.Play(Human, NPC);
+
+            txtMainMsg.Text = $"{Result}, NPC chose {NPC} while you chose {Human}!";
+        }
+
+        private void Rbn_Click(object sender, EventArgs e)
+        {
+            RadioButton tempRB = (RadioButton)sender;
+
+            Human = tempRB.Text;
+            txtMainMsg.Text = $"{Name}, You chose {Human}";
+
+            throw new NotImplementedException();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
